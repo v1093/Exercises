5150a24faadaf81c1d9a0f2a21b28f42ff8fcf86
veeru1093 <veeru.1093@gmail.com>
2017-12-01 00:44:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ def project_points(X, K, R, T, distortion_flag=False, distortion_params=None):
     """
     Your implementation goes here!
     """
-    X_camera = []
-    #img = cv.imread(X)  # Capture frame-by-frame
-    X_camera = cv.projectPoints(X, R, T, K, distortion_params)
-
+    X_camera, _ = cv.projectPoints(X, R, T, K, distortion_params)
 
     # Project points from 3d world coordinates to 2d image coordinates
     return X_camera
@@ -22,11 +19,16 @@ def project_and_draw(img, X_3d, K, R, T, distortion_flag, distortion_parameters)
     """
         Your implementation goes here!
     """
+
     # call your "project_points" function to project 3D points to camera coordinates
     cv.imshow("img", img)
     cv.waitKey()
+
     #X = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    X_camera1 = project_points(X_3d, K, R, T, distortion_parameters)
+    R, _ = cv.Rodrigues(R)
+
+
+    X_camera1 = project_points(X_3d, K, R, T, True, distortion_parameters)
     print(X_camera1)
 
     # draw the projected points on the image and save your output image here
@@ -48,6 +50,6 @@ if __name__ == '__main__':
     Kintr = data['intinsic_matrix']  # K matrix of the cameras
 
     imgs = [cv.imread(base_folder + str(i).zfill(5) + '.jpg') for i in range(TVecs.shape[0])]
-    #print(kc)
+
 
     project_and_draw(imgs[image_num], X_3D, Kintr, RMats[image_num], TVecs[image_num], True, kc)
